69820e88221916079e3509864c533a7efe7377c2
Tomás Antunes <tomasantunes@gmail.com>
2020-04-24 18:27:32 +0100

    Update

--- main.py
+++ main.py
@@ -1,63 +1,33 @@
 from flask import Flask, render_template, request, flash, jsonify, session, redirect, Markup
-from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 import datetime
-from werkzeug.utils import secure_filename
 import os
 
 def connect_db():
 	return sqlite3.connect("library.db")
 
 db = connect_db()
-LOGIN = False
-UPLOAD_FOLDER = '/home/user1/Documents/library-manager/upload'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.secret_key = "default_key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-def allowed_file(filename):
-   return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-	print(request.method)
-	if request.method == 'POST':
-		if 'file' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
-		file = request.files['file']
-
-		if file.filename == '':
-			flash('No selected file')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-	return "OK"
-
-
 
 def init():
 	db = connect_db()
 	c = db.cursor()
 
 	sql_books_table = """ CREATE TABLE IF NOT EXISTS books (
-							id integer PRIMARY KEY AUTOINCREMENT,
-							isbn text,
+							id integer PRIMARY KEY,
 							title text,
+							description text,
 							content text,
 							author text,
-							date date
+							date text
 						); """
 
 	c.execute(sql_books_table)
 
-@app.route("/")
-def home():
+def getBooksList():
 	db = connect_db()
 	c = db.execute('SELECT * FROM books')
 	rows = c.fetchall()
@@ -66,36 +36,94 @@ def home():
 	for row in rows:
 		book = {
 			'id': row[0],
-			'isbn': row[1],
-			'title': row[2],
+			'title': row[1],
+			'description' : Markup(row[2]),
 			'content' : Markup(row[3]),
-			'author' : Markup(row[4]),
-			'date' : datetime.datetime.strptime(row[4], '%Y-%m-%d %H:%M:%S.%f').strftime('%Y-%m-%d'),
+			'author' : row[4],
+			'date' : datetime.datetime.strptime(row[5], '%Y-%m-%d %H:%M:%S.%f').strftime('%Y-%m-%d'),
+			
 		}
+
 		books.append(book)
-
-	return render_template("home.html", books=books)
-
 	
+	return books
+
+def getBookById(id):
+	db = connect_db()
+	c = db.execute('SELECT * FROM books WHERE id = ?', [id])
+	rows = c.fetchall()
+	b = rows[0]
+
+	book = {
+		'id':b[0],
+		'title': b[1],
+		'description' : Markup(b[2]),
+		'content' : Markup(b[3]),
+		'author' : b[4],
+		'date' : datetime.datetime.strptime(b[5], '%Y-%m-%d %H:%M:%S.%f').strftime('%Y-%m-%d'),
+		'chapters' : [],
+	}
+
+	return book
+
+@app.route("/")
+def books():
+	books = getBooksList()
+	return render_template("books.html", books=books)
+
+@app.route("/new-book")
+def newBook():
+	return render_template("new-book.html")
+
+@app.route("/edit-book/<book>")
+def editBookById(book):
+	books = getBooksList()
+	return render_template("edit-book.html", book=book, books=books)
+
+@app.route("/get-book", methods=['GET'])
+def getBookByIdRoute():
+	id = request.args.get('id', "")
+	book = getBookById(id)
+	return jsonify(book)
+
+@app.route("/new-chapter/<book>")
+def newChapter(book):
+	return render_template("new-chapter.html", book=book)
+		
 @app.route("/add-book", methods=['POST'])
-def add_post():
+def addBook():
 	title = request.form.get('title', "")
 	author = request.form.get('author', "")
+	description = request.form.get('description', "")
 	content = request.form.get('content', "")
 
 	date = datetime.datetime.now()
 
-	if (title != "" and content != ""):
+	if (title != "" and author != "" and description != ""):
 		db = connect_db()
-		db.execute('INSERT INTO books (title, content, author, date) VALUES (?, ?, ?, ?)', [title, content, author, date])
+		db.execute('INSERT INTO books (title, author, description, date) VALUES (?, ?, ?, ?)', [title, author, description, content, date])
 		db.commit()
 		return redirect("/")
-	return redirect("/")
+	else:
+		return "Submission Invalid"
 
-@app.route("/book/<id>")
-def book(id):
-	return render_template("book.html", book=id)
+@app.route("/update-book", methods=['POST'])
+def updateBook():
+	id = request.form.get('id', "")
+	title = request.form.get('title', "")
+	author = request.form.get('author', "")
+	description = request.form.get('description', "")
 
+	date = str(datetime.datetime.now())
+
+	if (title != "" and author != "" and description != ""):
+		db = connect_db()
+		db.execute('UPDATE books SET title = ?, author = ?, description = ?, date = ? WHERE id = ?;', (title, author, description, id))
+		db.commit()
+		return redirect("/edit-book/" + id)
+	else:
+		return "Submission Invalid"
+ 
 if __name__ == "__main__":
 	init()
 	app.run()
